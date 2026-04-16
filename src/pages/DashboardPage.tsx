df3b397b2a2f7245267467004734482b45f8f2f7
owenkellyhilabs <owen.kelly@hilabs.ie>
2026-04-16 22:29:07 +0100

    wip, just adding a chart to see what its like

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,58 @@
 import type { FunctionComponent } from "react";
-
+import { CartesianGrid, Legend, Line, LineChart, XAxis, YAxis } from "recharts";
 interface DashboardPageProps {}
-
+const data = [
+  {
+    name: "Page A",
+    uv: 400,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 300,
+    pv: 4567,
+    amt: 2400,
+  },
+  {
+    name: "Page C",
+    uv: 320,
+    pv: 1398,
+    amt: 2400,
+  },
+  {
+    name: "Page D",
+    uv: 200,
+    pv: 9800,
+    amt: 2400,
+  },
+  {
+    name: "Page E",
+    uv: 278,
+    pv: 3908,
+    amt: 2400,
+  },
+  {
+    name: "Page F",
+    uv: 189,
+    pv: 4800,
+    amt: 2400,
+  },
+];
 const DashboardPage: FunctionComponent<DashboardPageProps> = () => {
-  return <div>Hello!</div>;
+  return (
+    <LineChart
+      style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+      responsive
+      data={data}
+    >
+      <CartesianGrid />
+      <Line dataKey="uv" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Legend />
+    </LineChart>
+  );
 };
 
 export default DashboardPage;
